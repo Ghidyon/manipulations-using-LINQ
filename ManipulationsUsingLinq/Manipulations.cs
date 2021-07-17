3ead8ac86a102684b7404fcbfe3d249d804b6857
Ghidyon <gideonakunana@gmail.com>
2021-07-17 20:40:32 +0200

    added matching characters to numbers

--- ManipulationsUsingLinq/Manipulations.cs
+++ ManipulationsUsingLinq/Manipulations.cs
@@ -12,7 +12,7 @@ namespace ManipulationsUsingLinq
         {
 
             // Query that shuffles sorted list of words
-            List<string> words = new List<string> { "lorem", "ipsum", "john", "doe", "lazy", "brown", "fox", "dinner", "nipple", "obi", "cubana" };
+            List<string> words = new() { "lorem", "ipsum", "john", "doe", "lazy", "brown", "fox", "dinner", "nipple", "obi", "cubana" };
             Console.WriteLine($"List of Words: { string.Join(", ", words)}");
 
             var random = new Random();
@@ -26,7 +26,34 @@ namespace ManipulationsUsingLinq
                               orderby random.Next()
                               select word;
             Console.WriteLine($"Random List of Words: {string.Join(", ", querySyntax)}");
+        }
 
+        public static void MatchAlphanumericCharacters()
+        {
+            Dictionary<char, int> charactersDictionary = new()
+            {
+                {')', 1 },
+                {'(', 2 },
+                {'*', 3 },
+                {'&', 4 },
+                {'^', 5 },
+                {'%', 6 },
+                {'$', 7 },
+                {'#', 8 },
+                {'@', 9 },
+                {'!', 0 }
+            };
+
+            Console.WriteLine("Input any of the following characters [ ), (, *, &, ^, %, $, #, @, ! ]");
+            string input = Console.ReadLine();
+            List<int> output = new();
+
+            foreach (char c in input)
+            {
+                output.Add(charactersDictionary[c]);
+            }
+
+            Console.WriteLine($"\nOutput: { string.Join("", output) }");
         }
     }
 }
--- ManipulationsUsingLinq/Program.cs
+++ ManipulationsUsingLinq/Program.cs
@@ -7,6 +7,7 @@ namespace ManipulationsUsingLinq
         static void Main(string[] args)
         {
             Manipulations.Run();
+            Manipulations.MatchAlphanumericCharacters();
         }
     }
 }
